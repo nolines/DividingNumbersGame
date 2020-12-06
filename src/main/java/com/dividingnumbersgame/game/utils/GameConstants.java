b5255fecd8262e5f851bab1f7c259f1053f71688
nolines <cemre.by@gmail.com>
2020-12-06 22:19:28 +0100

    GameConstant added

--- src/main/java/com/dividingnumbersgame/game/utils/GameConstants.java
+++ src/main/java/com/dividingnumbersgame/game/utils/GameConstants.java
@@ -4,5 +4,5 @@ public class GameConstants {
 
     public static final int WINNER_NUMBER = 1;
     public static final int DIVIDING_NUMBER = 3;
-    public static final int[] OPERATIONS = {-1, 0 ,1};
+    public static final int[] OPERATIONS = {0, -1, 1};
 }
